394f4a71f6cb8f6c3bb16713a828e47a054066cc
Loai Naghawi <loai.naghawi@realsoft-me.com>
2025-04-24 13:20:50 +0300

    cache and limit

--- Index.js
+++ Index.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 const logger = require("pino")();
 const Routes = require("./router.js");
+const rateLimit = require("express-rate-limit");
+const port = 3001;
+
+//limiter middleware
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  limit: 100,
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+});
+app.use(limiter);
 
 // logger middleware
 app.use((req, res, next) => {
@@ -12,4 +23,5 @@ app.use((req, res, next) => {
 });
 
 app.use(Routes);
-app.listen(3001);
+app.listen(port);
+console.log(`Application is running on port ${port}`);
--- controller.js
+++ controller.js
@@ -1,5 +1,7 @@
 import fetch from "node-fetch";
 import "dotenv/config";
+import NodeCache from "node-cache";
+const myCache = new NodeCache();
 
 export async function getMain(req, res, next) {
   res.end("Hello!");
@@ -12,16 +14,24 @@ export async function getNotFound(req, res, next) {
 
 export async function getFruits(req, res, next) {
   try {
+    let data;
     let fruitName = JSON.stringify(req.query.fruit);
     fruitName = fruitName.substring(1, fruitName.length - 1);
-    const response = await fetch(
-      `${process.env.FRUIT_URL}${fruitName || "all"}`
-    );
-    const data = await response.json();
-    if (data.error) {
-      res.end("Selected fruit does not exist");
-      return;
+    let inCache = myCache.has(fruitName || "all");
+    if (!inCache) {
+      const response = await fetch(
+        `${process.env.FRUIT_URL}${fruitName || "all"}`
+      );
+      data = await response.json();
+      myCache.set(fruitName || "all", data, 300);
+      if (data.error) {
+        res.end("Selected fruit does not exist");
+        return;
+      }
+    } else {
+      data = myCache.get(fruitName || "all");
     }
+    console.log(inCache);
     res.json(data);
   } catch {
     res.status(400);
@@ -31,15 +41,26 @@ export async function getFruits(req, res, next) {
 
 export async function getGames(req, res, next) {
   let category = JSON.stringify(req.query.category);
-  req.query.category == null
-    ? (category = "")
-    : (category = "?category=" + category.substring(1, category.length - 1));
-  const response = await fetch(`${process.env.GAME_URL}${category}`);
-  const data = await response.json();
+  let data;
+  let cachedCategory = category;
+  let inCache = myCache.has(cachedCategory || "all");
+  if (!inCache) {
+    req.query.category == null
+      ? (category = "")
+      : (category = "?category=" + category.substring(1, category.length - 1));
+    const response = await fetch(`${process.env.GAME_URL}${category}`);
+    data = await response.json();
+    console.log(cachedCategory);
+    myCache.set(cachedCategory || "all", data);
+  } else {
+    data = myCache.get(cachedCategory || "all");
+  }
+  console.log(inCache);
   res.json(data);
 }
 
 export async function getRecipes(req, res, next) {
+  let data;
   const options = {
     method: "GET",
     headers: {
@@ -50,39 +71,68 @@ export async function getRecipes(req, res, next) {
   };
   const key = "&app_key=" + process.env.FOOD_KEY;
   const id = "&app_id=" + process.env.FOOD_ID;
-  const response = await fetch(`${process.env.FOOD_URL}${id}${key}`, options);
-  const data = await response.json();
-  if (data.status == "error") {
-    res.status(401);
-    res.end(
-      "Unauthorized! Make sure you have the correct userID, appID, and appKey"
-    );
+  let inCache = myCache.has("Recipe");
+  if (!inCache) {
+    const response = await fetch(`${process.env.FOOD_URL}${id}${key}`, options);
+    data = await response.json();
+
+    if (data.status == "error") {
+      res.status(401);
+      res.end(
+        "Unauthorized! Make sure you have the correct userID, appID, and appKey"
+      );
+    }
+    myCache.set("Recipe", data);
+  } else {
+    data = myCache.get("Recipe");
   }
+  console.log(inCache);
   res.json(data);
 }
 
 export async function getMix(req, res, next) {
   let result = [];
-  const responseFood = await fetch(`${process.env.FRUIT_URL}banana`);
-  const dataFood = await responseFood.json();
+  let dataFood;
+  let dataGames;
+  let dataRecipe;
+  let foodCache = myCache.has("banana");
+  let gameCache = myCache.has(`"shooter"`);
+  let recipeCache = myCache.has("Recipe");
+  if (!foodCache) {
+    const responseFood = await fetch(`${process.env.FRUIT_URL}banana`);
+    dataFood = await responseFood.json();
+    myCache.set("banana", dataFood);
+  } else dataFood = myCache.get("banana");
   result.push(dataFood);
-  let category = "?category=shooter";
-  const responseGames = await fetch(`${process.env.GAME_URL}${category}`);
-  const dataGames = await responseGames.json();
+  if (!gameCache) {
+    let category = "?category=shooter";
+    const responseGames = await fetch(`${process.env.GAME_URL}${category}`);
+    dataGames = await responseGames.json();
+    myCache.set("shooter", dataGames);
+  } else {
+    dataGames = myCache.get("shooter");
+  }
   result.push(dataGames);
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      "Edamam-Account-User": process.env.FOOD_USER,
-      "Accept-Language": "en",
-    },
-  };
-  const key = "&app_key=" + process.env.FOOD_KEY;
-  const id = "&app_id=" + process.env.FOOD_ID;
-  const response = await fetch(`${process.env.FOOD_URL}${id}${key}`, options);
-  const data = await response.json();
-  result.push(data);
-  console.log(result);
+  if (!recipeCache) {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "Edamam-Account-User": process.env.FOOD_USER,
+        "Accept-Language": "en",
+      },
+    };
+    const key = "&app_key=" + process.env.FOOD_KEY;
+    const id = "&app_id=" + process.env.FOOD_ID;
+    const response = await fetch(`${process.env.FOOD_URL}${id}${key}`, options);
+    dataRecipe = await response.json();
+    myCache.set("Recipe", dataRecipe);
+  } else {
+    dataRecipe = myCache.get("Recipe");
+  }
+  result.push(dataRecipe);
+  console.log(
+    `Food Cache: ${foodCache},   Games Cache: ${gameCache},   Recipe Cache: ${recipeCache}`
+  );
   res.json(result);
 }
